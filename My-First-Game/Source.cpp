2ac4206f460224aa703d498e3078885695df477f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-03 20:01:29 +0300

    add int data type

--- My-First-Game/Source.cpp
+++ My-First-Game/Source.cpp
@@ -5,6 +5,9 @@ int main()
 	char myCharacter;
 	myCharacter = 'y';
 	/* Single quote kullandim char da singe kullanilir unutma! Double quote stringi temsil eder. */
+
+	int myInt;
+	myInt = 23;
 	
 	
 	system("pause");
