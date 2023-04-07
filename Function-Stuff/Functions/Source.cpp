086f72225af9d997afbde6b5e4eea4a29c7ded12
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-07 20:01:57 +0300

    add function with a return (i mean the function that i write int ...(), not void...()

--- Function-Stuff/Functions/Source.cpp
+++ Function-Stuff/Functions/Source.cpp
@@ -12,10 +12,20 @@ void printNumber(int number1, int number2)
 }
 
 
+int add(int a, int b)
+{
+	int result;
+	result = a + b;
+	cout << "The result is: " << result << endl;
+	return result;
+}
+
+
 int main()
 {
 	welcome();
 	printNumber(5, 3);
+	add(2, 9);
 
 	system("pause");
 }
