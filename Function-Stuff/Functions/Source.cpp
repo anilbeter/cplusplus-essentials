98986c0587e873bba0583f4c1001be21aab1f05d
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-07 19:58:39 +0300

    add a function with parameters

--- Function-Stuff/Functions/Source.cpp
+++ Function-Stuff/Functions/Source.cpp
@@ -6,9 +6,16 @@ void welcome()
 	cout << "Welcome to the game!\n";
 }
 
+void printNumber(int number1, int number2) 
+{
+	cout << number1 + number2 << endl;
+}
+
+
 int main()
 {
 	welcome();
+	printNumber(5, 3);
 
 	system("pause");
 }
