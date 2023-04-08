ea5137ea2e1e800033275457a20b57011ffd093e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-08 10:50:46 +0300

    add simple counter with While Loop

--- While Loops/Source.cpp
+++ While Loops/Source.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
+	int myInt(0);
 
+	int count = 0;
 
+	while (count < 10)
+	{
+		cout << myInt << endl;
+		count++;
+	}
+	/* myInt variable'ýný (yani 0'ý) 10 defa yazdýran basit bir while loopu */
 	system("pause");
 }
