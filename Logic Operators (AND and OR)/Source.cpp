fc6f4c3fcd40e975a0a91ca38ec098b241fb33a1
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-09 21:29:08 +0300

    add some logical operators (ADD and OR operators)

--- Logic Operators (AND and OR)/Source.cpp
+++ Logic Operators (AND and OR)/Source.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
+	/* && --> AND operator */
+	/* || --> OR operator */
 
+	int i = 1;
+	int j = 2;
+	int k = 1;
+
+	bool Condition = true;
+	
+	if (i == j || i == k)
+	{
+		cout << Condition << endl;
+		/* Eðer doðruysa beklenent output ---> 1 */
+	}
+	else
+	{
+		Condition = false;
+		cout << Condition << endl;
+		/* Eðer yanlýþsa beklenen output ----> 0 */
+	}
+
+	string myName = "Anil";
+	int myAge = 23;
+
+	if ((myName == "Anil") && (myAge == 23))
+	{
+		cout << "My name is Anil " << "and " << "I'm 23 Years old." << endl;
+	}
+	/* Beklenen output --> My name is Anil and I'm 23 Years old. */
 
 
 	system("pause");
