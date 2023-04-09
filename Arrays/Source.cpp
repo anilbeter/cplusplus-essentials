93735ab24ba1539c426bdb86c3398dceeb789b86
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-09 22:15:28 +0300

    create our very first and simple array

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
+	int MyIntArray[10]{};
 
+	for (int i = 0; i < 10; i++)
+	{
+		MyIntArray[i] = i;
+	}
+			
+	cout << "The first element in MyIntArray is: " << MyIntArray[0] << endl;
+	cout << "The fifth element is: " << MyIntArray[4] << endl;
 
 	system("pause");
 }
