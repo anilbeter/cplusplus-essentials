b1724cce449c31e7650fe33fefadabd2aab2cb37
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-09 20:34:08 +0300

    add C-Style string and C++ style string (we use C++)

--- Strings/Source.cpp
+++ Strings/Source.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main()
 {
+	/* C-style (old school) string like this:  */
+	char myString[5] = { 'A', 'n', 'i', 'l', '\0' };
+	cout << myString << endl;
+	/* Output --> Anil */
 
+	/* with <string> built-in library, i can easily create string: */
+	string myFirstName = "Anil";
+	string mySecondName = "Beter";
+	string space = " ";
+	cout << "My full name is " << myFirstName << space << mySecondName << endl;
+	/* Output ---> My full name is Anil Beter */
 
 
 	system("pause");
