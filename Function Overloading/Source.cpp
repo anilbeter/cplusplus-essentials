c79a53e3d52682890df3ae036e6d33221d696445
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-09 10:33:45 +0300

    add simple print function

---
+++ Function Overloading/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void Print(string str);
+
+int main()
+{
+	Print("Hello World!");
+
+
+
+	system("pause");
+}
+
+void Print(string str)
+{
+	cout << str << endl;
+}
