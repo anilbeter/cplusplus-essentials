cf1e7f88594d96db06ebe7a4b8c657227b4766e9
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-09 09:05:52 +0300

    add nested for loops

--- For Loops/Source.cpp
+++ For Loops/Source.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
-	int i = 0;
-	for (i; i <= 10; i++)
+	for (int i = 0; i <= 10; i++)
 	{
-		cout << i << endl;
+		for (int j = 0; j <= 10; j++)
+		{
+			cout << "i = " << i << ", and j = " << j << endl;
+		}
 	}
 
 	system("pause");
