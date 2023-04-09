77d5836c02dc72f56b4d2cb82af8eaa8421fff95
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-09 09:58:56 +0300

    use '#include <string>' syntax and add comment about why we add it

--- References/Source.cpp
+++ References/Source.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <string>
+/* #include <string>, string tanýmlamama olanak asðlýyor */
 using namespace std;
 
 int main()
 {
-
+	string myStr = "Anil";
+	cout << myStr << endl;
 
 	system("pause");
 }
