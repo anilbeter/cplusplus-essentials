9b08e435c83c7a178fd91b5d30b3dea18fd17ed7
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-09 10:34:49 +0300

    i overload the function! lets goooo

--- Function Overloading/Source.cpp
+++ Function Overloading/Source.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 void Print(string str);
+void Print(int i);
 
 int main()
 {
 	Print("Hello World!");
+	Print(23);
 
 
 
@@ -16,4 +18,9 @@ int main()
 void Print(string str)
 {
 	cout << str << endl;
+}
+
+void Print(int i)
+{
+	cout << i << endl;
 }
