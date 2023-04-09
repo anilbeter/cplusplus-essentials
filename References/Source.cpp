73489351145134ef08323abc823872cf1b63d3b0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-09 10:01:35 +0300

    add reference variable

--- References/Source.cpp
+++ References/Source.cpp
@@ -6,7 +6,15 @@ using namespace std;
 int main()
 {
 	string myStr = "Anil";
+
+	string& myRef = myStr;
+	/* & koyarak ref variable oluþturdum. myReF = Anil oldu çünkü myStr = Anil, referans aldýðýmýz variable myRef */
+	
+	
 	cout << myStr << endl;
+	/*Anil*/
+	cout << myRef << endl;
+	/*Anil*/
 
 	system("pause");
 }
