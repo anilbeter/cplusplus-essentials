f308abbb3463530c4554c66fe32bc29922930575
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-11 20:49:49 +0300

    write another SWITCH STATEMENT

--- Switch Statements/Source.cpp
+++ Switch Statements/Source.cpp
@@ -14,6 +14,8 @@ const float CrouchSpeed = 350.f;
 
 void UpdateMovementSpeed(PlayerStatus P_Status, float& speed);
 
+void SwitchStatementWithInt(int i);
+
 
 
 int main()
@@ -25,6 +27,9 @@ int main()
 	UpdateMovementSpeed(status, MovementSpeed);
 	cout << "Movement speed is: " << MovementSpeed << endl;
 	cout << "The player status: " << status << endl;
+
+	SwitchStatementWithInt(23);
+	// Output ---> I LOVE 23! 23 IS MY TRANSFORMATION
 
 
 	system("pause");
@@ -42,6 +47,20 @@ void UpdateMovementSpeed(PlayerStatus P_Status, float& speed)
 		break;
 	case Ps_Crouching:
 		speed = CrouchSpeed;
+	}
+}
+
+void SwitchStatementWithInt(int i)
+{
+	switch (i)
+	{
+	case 0:
+		cout << "Hmm your number is 0" << endl;
 		break;
+	case 1:
+		cout << "1 is nice number tho" << endl;
+		break;
+	case 23:
+		cout << "I LOVE 23! 23 IS MY TRANSFORMATION" << endl;
 	}
 }
