16fa36faa325d2e6358b8d296d818b024165695c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-11 11:49:39 +0300

    ENUM'a manuel olarak değer atadım ve comment ile açıkladım

--- Enums/Source.cpp
+++ Enums/Source.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 
 enum PlayerStatus {
-	PS_Crouched,  /*0*/
-	PS_Standing,  /*1*/
-	PS_Walking,	  /*2*/	
-	PS_Running	  /*3*/
+	PS_Crouched = 1,  /* Default olarak PS_Crouched deðeri 0 dý ama manuel olarak 1 atadým böyle bi þey yapma hakkým var, 1000 de atayabilirim */
+	PS_Standing, /* Üsttekini 1 olarak atadýðým için bu otomatik olarak 2 oldu (Formül = her defasýnda 1 artarak gidiyor) */
+	PS_Walking, /* Bu da 3 oldu... */
+	PS_Running
 };
 
 int main()
