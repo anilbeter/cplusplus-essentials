ec1eb616375136bee641c3604ef98a302e434844
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-11 11:46:50 +0300

    add our very first ENUM

--- Enums/Source.cpp
+++ Enums/Source.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+
+enum PlayerStatus {
+	PS_Crouched,  /*0*/
+	PS_Standing,  /*1*/
+	PS_Walking,	  /*2*/	
+	PS_Running	  /*3*/
+};
 
 int main()
 {
