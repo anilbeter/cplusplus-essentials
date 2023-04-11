ef40cff0d0ec0c22e0cfd998f0f96509563831c2
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-11 20:26:41 +0300

    write line of code with IF / ELSE IF STATEMENTS instead of SWITCH STATEMENT

--- Switch Statements/Source.cpp
+++ Switch Statements/Source.cpp
@@ -2,11 +2,46 @@
 #include <string>
 using namespace std;
 
+enum PlayerStatus {
+	PS_Running,
+	PS_Walking,
+	Ps_Crouching
+};
+
+const float RunSpeed = 800.f;
+const float WalkSpeed = 500.f;
+const float CrouchSpeed = 350.f;
+
+void UpdateMovementSpeed(PlayerStatus P_Status, float& speed);
+
+
 
 int main()
 {
+	float MovementSpeed;
 
+	PlayerStatus status = PS_Walking;
+
+	UpdateMovementSpeed(status, MovementSpeed);
+	cout << "Movement speed is: " << MovementSpeed << endl;
+	cout << "The player status: " << status << endl;
 
 
 	system("pause");
+}
+
+void UpdateMovementSpeed(PlayerStatus P_Status, float& speed)
+{
+	if (P_Status == PS_Running)
+	{
+		speed = RunSpeed;
+	}
+	else if (P_Status == PS_Walking)
+	{
+		speed = WalkSpeed;
+	}
+	else if (P_Status == Ps_Crouching)
+	{
+		speed = CrouchSpeed;
+	}
 }
