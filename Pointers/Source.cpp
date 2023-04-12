25317158d4ecc18ad87acdde8074dd4f1872fba0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-12 15:24:58 +0300

    use POINTERS with ARRAYS

--- Pointers/Source.cpp
+++ Pointers/Source.cpp
@@ -5,14 +5,27 @@ using namespace std;
 
 int main()
 {
-	int a = 23;
-	int* aPointer = &a;
+	int numbers[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	cout << aPointer << endl;
-	/* Output ---> 000000879090FBC4 */
+	int* NumPtr = numbers;
+	/* Pointerý bir array'e eþitlersem otomatik olarak arrayin ilk (0. indexini) indexini referans alacak. Yani NumPtr bu durumda 0 a eþit olucak */
 
-	cout << *aPointer << endl;
-	/* Output ----> 23 */
+	cout << *NumPtr << endl;
+	// Output ---> 0
+
+	/* NumPtr++ yazarak bunu deðiþtirebilirim, NumPtr++ yazarsam hemen bi yanýndaki indexe geçicek, yani bizim durumumuzda 1 */
+
+	NumPtr++;
+	cout << *NumPtr << endl;
+	// Output ---> 1
+
+	NumPtr++;
+	cout << *NumPtr << endl;
+	// Output ---> 2
+
+	NumPtr += 3; /* 2 numaralý indexi gösteriyordu, 3 ekledim 5 oldu. 5 numaralý index = 5 */
+	cout << *NumPtr << endl;
+	// Output ---> 5
 
 	system("pause");
 }
