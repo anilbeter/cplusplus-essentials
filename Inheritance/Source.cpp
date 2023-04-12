0af6f6511b83a8f2bebc471fd6c0a74541e2d861
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-12 19:42:57 +0300

    (INHERITANCE)write some code that includes CLASS and CONSTRUCTORS

--- Inheritance/Source.cpp
+++ Inheritance/Source.cpp
@@ -2,11 +2,41 @@
 #include <string>
 using namespace std;
 
+class Animal
+{
+public:
+	Animal();
+
+	string Name;
+	int Age;
+	int NumberOfLimbs;
+
+	void Report();
+};
+
 
 int main()
 {
 
+	Animal animal;
 
+	animal.Report();
 
-	system("pause;");
+	system("pause");
+}
+
+Animal::Animal()
+{
+	cout << "An animal is borned!\n";
+
+	Name = "Default";
+	Age = 2;
+	NumberOfLimbs = 4;
+}
+
+void Animal::Report()
+{
+	cout << "Animal's name is " << Name << endl;
+	cout << "Animal's age is " << Age << endl;
+	cout << "Animal's limbs number is " << NumberOfLimbs << endl;
 }
