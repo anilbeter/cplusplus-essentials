859a3082cb23d4b6161bd51cd902cfa4b0d75a1b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-12 08:07:49 +0300

    write code with my first STRUCT

--- Structs/Source.cpp
+++ Structs/Source.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
+struct Player
+{
+	int Level;
+	float Health;
+	float Damage;
+	float Stamina;
+	void TakeDamage(float dmg)
+	{
+		Health -= dmg;
+	}
+	int GetLevel()
+	{
+		if (Level > 10)
+		{
+			cout << "Your level greater than 10! \n";
+		}
+		return Level;
+	}
+
+};
 
 int main()
 {
+	Player p_1;
+	p_1.Level = 12;
+	p_1.Health = 100.f;
+	p_1.Damage = 10.f;
+	p_1.Stamina = 20.f;
+
+	cout << "p_1 level = " << p_1.GetLevel() << endl;
+
+	p_1.TakeDamage(40.f);
 
 
+	cout << "p_1 takes " << 40.f << " damage!" << endl;
+	cout << "p_1 health = " << p_1.Health << endl;
 
 	system("pause");
 }
