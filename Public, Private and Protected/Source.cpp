f4e20a7ea0f578d9cd9eabc9f6176e6a8ac6e90d
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-04-14 08:35:28 +0300

    write some code includes private and public keyword

--- Public, Private and Protected/Source.cpp
+++ Public, Private and Protected/Source.cpp
@@ -2,11 +2,28 @@
 #include <string>
 using namespace std;
 
+class Creature
+{
+public:
+	Creature();
+
+private:
+	float Health;
+	string Name;
+};
+
 
 int main()
 {
 
-
+	Creature Igor;
+	/* Output ---> A creature has been created! */
 
 	system("pause");
+}
+
+
+Creature::Creature()
+{
+	cout << "A creature has been created!\n";
 }
